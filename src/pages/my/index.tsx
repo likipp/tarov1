e450df5815e2bc95540e8fe2cd3b4d8d3434cd16
xiaom.liu <likipp@163.com>
2021-12-22 16:57:11 +0800

    init

---
+++ src/pages/my/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { View, Text } from "@tarojs/components";
+import { Cell, Grid, GridItem, Image } from "@antmjs/vantui";
+
+const My = () => {
+    const [login, setLogin] = useState(false)
+    return (
+        <View>
+            {
+                login ? <View>已登录</View>
+                : <View style={{
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    marginBottom: '10px'
+                }}>
+                        <Image
+                            round={ true }
+                            width="5rem"
+                            height="5rem"
+                            src="/pages/static/images/userL.png"
+                        />
+                        <Text style={{marginLeft: '10px'}}>请登录</Text>
+                </View>
+            }
+            <Cell
+                isLink={ true }
+                icon="balance-list"
+                title="购买"
+                linkType="navigateTo"
+                value={"订单11"}
+                url="/pages/dashboard/index"
+            />
+            <Grid columnNum={3} border={false} style={{borderBottom: '1px solid #f8f8f9'}}>
+                <GridItem
+                    icon="credit-pay"
+                    text="待支付"
+                    />
+                <GridItem
+                    icon="shop"
+                    text="待发货"
+                />
+                <GridItem 
+                    icon="logistics"
+                    text="已发货"
+                />
+            </Grid>
+            <Cell
+                isLink={ true }
+                icon="location"
+                title="地址管理"
+                linkType="navigateTo"
+                url="/pages/dashboard/index"
+            />
+        </View>
+    )
+}
+
+export default My
