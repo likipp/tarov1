ef3341c1a5d27cbbecfcda9e29d61759322c1e66
xiaom.liu <likipp@163.com>
2022-01-08 16:34:26 +0800

    完善各项功能

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -19,6 +19,88 @@ const Product = () => {
     const [length, setLength] = useState(0)
     const [params, setParams] = useState({title: "", key: 1})
 
+    const [products, setProducts] = useState(
+      [
+        {
+          title: "花喜",
+          list: [{
+            id: 1,
+            mark: "",
+            p_name: "花喜酵素",
+            p_number: "HX0001",
+            p_spec: "",
+            picture: "http://127.0.0.1:8080/images/花喜/2018081709312590.jpg",
+            purchase_price: 150,
+            sale_price: 298,
+            unit: 3,
+            updateBy: "",
+            updatedAt: "2021-11-25T08:25:21.656+08:00",
+            ware_house: 2
+          },
+          {
+            id: 5,
+            mark: "",
+            p_name: "古方湿清茶",
+            p_number: "HX0002",
+            p_spec: "",
+            picture: "http://127.0.0.1:8080/images/花喜/2018081709322284.jpg",
+            purchase_price: 150,
+            sale_price: 398,
+            unit: 3,
+            updateBy: "",
+            updatedAt: "2021-12-11T09:24:22.483+08:00",
+            ware_house: 2,
+          },
+          {
+            id: 6,
+            mark: "",
+            p_name: "花喜高纤谷物棒（烧烤味）",
+            p_number: "HX0003",
+            p_spec: "",
+            picture: "http://127.0.0.1:8080/images/1/20211211152515.png",
+            purchase_price: 30,
+            sale_price: 60,
+            unit: 3,
+            updateBy: "",
+            updatedAt: "2021-12-11T15:25:16.049+08:00",
+            ware_house: 2,
+          }
+        ]
+        },
+        {
+        title: "百诺恩",
+        list: [{
+          id: 1,
+          p_name: "百诺恩面巾纸",
+          p_number: "B00001",
+          p_spec: "",
+          picture: "/favicon.ico",
+          purchase_price: 10,
+          sale_price: 25,
+          unit: 1,
+          updateBy: "",
+          updatedAt: "2021-11-25T08:27:01.888+08:00",
+          ware_house: 3,
+        },
+        {
+          id: 3,
+          mark: "",
+          p_name: "百诺恩阳光棉亲肌瞬干纸尿裤",
+          p_number: "B00002",
+          p_spec: "",
+          picture: "/favicon.ico",
+          purchase_price: 100,
+          sale_price: 225,
+          unit: 1,
+          updateBy: "",
+          updatedAt: "2021-11-27T14:15:24.203+08:00",
+          ware_house: 3,
+        }
+        ]
+      }
+    ]
+    )
+      
     
 
     useEffect(() => {
@@ -108,12 +190,29 @@ const Product = () => {
               style={{width: "100px", flex: "1 0 auto"}}
             >
               {
-                data?.map((item) => {
-                  return <SidebarItem title={item.title} className={data[activeKey].title === item.title ? "myactive" : "myunactive"}/>
+                products?.map((item) => {
+                  return <SidebarItem title={item.title} className={products[activeKey].title === item.title ? "myactive" : "myunactive"}/>
                 })
               }
             </Sidebar>
-            <View style={{flex: "2 0 auto", backgroundColor: "#e8eaec"}}>
+            {
+              products?.map((item) => {
+                return (
+                  <View style={{flex: "2 0 auto", backgroundColor: "#e8eaec"}}>
+                    <View style={{marginLeft: "10px", marginTop: "15px", color: "#515a6e", backgroundColor: "#e8eaec"}}>
+                      <View style={{marginBottom: "15px"}}>
+                          <Text style={{display: "block"}}>{item.title}</Text>
+                          <Text style={{display: "block"}}>共{item.list?.length}个商品</Text>
+                      </View>
+                      <View>
+                        <ProductItem list={item.list} height={height} />
+                      </View>
+                    </View>
+                </View>
+              )
+            })
+          }
+            {/* <View style={{flex: "2 0 auto", backgroundColor: "#e8eaec"}}>
               <View style={{marginLeft: "10px", marginTop: "15px", color: "#515a6e", backgroundColor: "#e8eaec"}}>
                 <View style={{marginBottom: "15px"}}>
                     <Text style={{display: "block"}}>{params.title}</Text>
@@ -123,7 +222,8 @@ const Product = () => {
                   <ProductItem list={list} height={height} />
                 </View>
               </View>
-            </View>
+            </View> */}
+            
           </View>
           <View style={{width: "100%", backgroundColor: "#fafafa", height: "100px", display:"flex", alignItems: "center", justifyContent:"space-between", borderTop: "1px solid #e8eaec", zIndex: 999, position: "fixed", bottom:0}}>
               {
